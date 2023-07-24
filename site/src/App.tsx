ff2090a6f130ed41876a5039c0c95b1de7e3cdc0
Adam Skoufis <adam.skoufis@gmail.com>
2023-07-24 22:15:00 +1000

    Get Vanilla Extract working, upgrade deps, go full ESM

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 // @ts-expect-error
 import reactLogo from './assets/react.svg';
-import './App.css';
-import { Box } from 'system';
+import { Box } from '@askoufis/system';
+import { sprinkles } from '@askoufis/system/css';
+import * as styles from './App.css';
+import clsx from 'clsx';
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -11,21 +13,38 @@ const App = () => {
     <div className='App'>
       <div>
         <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
+          <img
+            src='/vite.svg'
+            className={clsx([styles.logo, styles.viteHover])}
+            alt='Vite logo'
+          />
         </a>
         <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
+          <img
+            src={reactLogo}
+            className={clsx([styles.logo, styles.reactHover, styles.logoSpin])}
+            alt='React logo'
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className='card'>
+      <div
+        className={sprinkles({
+          color: 'blue-200',
+          background: 'gray-700',
+          paddingTop: 'large',
+        })}
+      >
+        foo
+      </div>
+      <div className={styles.card}>
         <button onClick={() => setCount((c) => c + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
         <Box />
       </div>
-      <p className='read-the-docs'>
+      <p className={styles.readTheDocs}>
         Click on the Vite and React logos to learn more. A change.
       </p>
     </div>
--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'dist');
@@ -8,7 +9,7 @@ const outDir = resolve(__dirname, 'dist');
 // https://vitejs.dev/config/
 export default defineConfig({
   root,
-  plugins: [react()],
+  plugins: [react(), vanillaExtractPlugin()],
   build: {
     outDir,
     emptyOutDir: true,
