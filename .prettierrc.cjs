923814789b36044a890a97910717f9b3dd14b352
Adam Skoufis <adam.skoufis@gmail.com>
2023-07-30 14:33:25 +1000

    Get language server and prettier formatting working

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,4 +1,14 @@
+/** @type {import('prettier').Config} */
 module.exports = {
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
   tabWidth: 2,
   useTabs: false,
   semi: true,
