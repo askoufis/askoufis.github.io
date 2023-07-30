bab57b794784e96fef08d80159de96834d976a90
Adam Skoufis <adam.skoufis@gmail.com>
2023-07-30 13:22:23 +1000

    Add Vanilla Extract integration to site

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,5 +1,10 @@
 // eslint-disable-next-line import/no-unresolved
 import { defineConfig } from 'astro/config';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [vanillaExtractPlugin({ identifiers: 'short' })],
+  },
+});
---
+++ site/src/components/Card.css.ts
@@ -0,0 +1,3 @@
+import { style } from '@vanilla-extract/css';
+
+export const cardStyle = style({ color: 'red' });
